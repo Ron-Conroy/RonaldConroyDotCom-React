4d3b6759fe159649816db812ce41a1a58f0169f9
Ronald Conroy <ron@ronaldconroy.com>
2021-02-02 19:24:49 -0500

    Edit Intro verbiage to state site is WIP

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -4,7 +4,7 @@ export default class introduction extends React.Component {
     render() {
         return (
             <div>
-                Introduction section placeholder.
+                Welcome to my Portfolio site! This site is currently under construction as I am actively learning React & AWS. This site is a React application being powered by AWS Amplify w/ automatic deploys.
             </div>
         );
     }
